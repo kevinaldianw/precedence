4ed0ffdf0b315d23b9d314f6ff91c90b07e8bb15
kevin aldian <kevin19aldian98@gmail.com>
2020-05-11 16:36:13 +0700

    Versi 1.0.123

---
+++ precedence.c
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 10 + 11 - 20 / 11 * 40 % 25 & 35 >> 2 | 25 && 20 << 1;
+    printf("%d",a);
+    return 0;
+}
